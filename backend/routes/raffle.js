ea5419d50a4e5023520becf976d01101dc8fdd5c
unknown <evandrosimendes@gmail.com>
2017-12-27 19:37:59 -0200

    repair app

--- backend/routes/raffle.js
+++ backend/routes/raffle.js
@@ -5,48 +5,49 @@ const Participant = require('../models/Participant.js');
 const api_key = 'key-2aa1b759638496b6a1f56b9257d76940';
 const domain = 'simple-secret-friend.herokuapp.com';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
- 
+
+const htmlEmail = (who) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url('https://simple-secret-friend.herokuapp.com/assets/background.jpg');
+            background-repeat: no-repeat;
+            background-origin: content-box;
+            background-attachment: fixed;
+            background-position: center; 
+            background-size: auto;
+        }
+        h1{
+            color: whitesmoke;
+            padding-top: 30%;
+            padding-left: 31%;
+        } 
+        h2{
+            color: whitesmoke;
+            padding-top: 1%;
+            padding-left: 45%;
+        }
+    </style>
+</head>
+<body>
+    <div>
+    <h1>Seu amigo secreto é</h1>
+    <h2>${who}</h2>
+    </div>
+
+</body>
+</html>`;
 const mail =  (to,who) => { 
   return {
     from: 'Chief of Secret <evandro.s.i.mendes@hotmail.com>',
     to: to,
     subject: 'Secret Friend',
-    text: `<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>Document</title>
-                <style>
-                    body {
-                        background-image: url('https://simple-secret-friend.herokuapp.com/assets/background.jpg');
-                        background-repeat: no-repeat;
-                        background-origin: content-box;
-                        background-attachment: fixed;
-                        background-position: center; 
-                        background-size: auto;
-                    }
-                    h1{
-                        color: whitesmoke;
-                        padding-top: 30%;
-                        padding-left: 31%;
-                    } 
-                    h2{
-                        color: whitesmoke;
-                        padding-top: 1%;
-                        padding-left: 45%;
-                    }
-                </style>
-            </head>
-            <body>
-                <div>
-                <h1>Seu amigo secreto é</h1>
-                <h2>${who}</h2>
-                </div>
-            
-            </body>
-            </html>`
+    text: htmlEmail(who)
   }
 }; 
 
@@ -68,7 +69,6 @@ router.post('/', (req, res, next) => {
       })
     })
 });
-
 
 
 module.exports = router;
